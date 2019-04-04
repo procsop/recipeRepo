6fcf3249512e59fb055dfbd035cb4dd6312a0039
dnemeth <neduabi@sztaki.mta.hu>
2019-04-04 14:23:13 +0200

    Userdetails extended with info

--- src/main/java/com/recipe/service/UserDetailsImpl.java
+++ src/main/java/com/recipe/service/UserDetailsImpl.java
@@ -11,6 +11,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+/**
+ * This class manages all data of users.
+ */
 public class UserDetailsImpl implements UserDetails {
 
 	//private static final long serialVersionUID = 3185970362329652822L;
@@ -20,7 +23,9 @@ public class UserDetailsImpl implements UserDetails {
 	public UserDetailsImpl(User user) {
 		this.user = user;
 	}
-
+	/**
+	 * Collection of users.
+	 */
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		Collection<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
@@ -31,36 +36,57 @@ public class UserDetailsImpl implements UserDetails {
 		return authorities;
 	}
 
+	/**
+	 * Get user password.
+	 */
 	@Override
 	public String getPassword() {
 		return user.getPassword();
 	}
 
+	/**
+	 * Get username.
+	 */
 	@Override
 	public String getUsername() {
 		return user.getEmail();
 	}
 
+	/**
+	 * Get if account is expired or not.
+	 */
 	@Override
 	public boolean isAccountNonExpired() {
 		return true;
 	}
 
+	/**
+	 * Get if account is locked.
+	 */
 	@Override
 	public boolean isAccountNonLocked() {
 		return true;
 	}
 
+	/**
+	 * Get if credentials are expired.
+	 */
 	@Override
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
 
+	/**
+	 * Get if login is enabled.
+	 */
 	@Override
 	public boolean isEnabled() {
 		return true;
 	}
 
+	/**
+	 * Get whole user object.
+	 */
 	public User getUser() {
 		return this.user;
 	}
