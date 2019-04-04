971cad7a87648efb6562403b264614a3b4eea6a1
dnemeth <neduabi@sztaki.mta.hu>
2019-04-04 14:18:25 +0200

    Some infos about RecipeRepo interface

--- src/main/java/com/recipe/repo/RecipeRepository.java
+++ src/main/java/com/recipe/repo/RecipeRepository.java
@@ -1,20 +1,31 @@
 package com.recipe.repo;
 
 import java.util.List;
-
 import com.recipe.entity.Recipe;
-
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Interface for recipes.
+ */
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 	List<Recipe> findAll();
 
 	//List<Ingredient> findIngredientByRecipeId(Long id);
-
+    /**
+     * Fint recipe by id.
+     * @param selectedRecipeId
+     * @return
+     */
 	Recipe findById(Long selectedRecipeId);
 	//Ingredient findByIngredientsIngredient(String string);
 
-	List<Recipe> findAllByOrderByIngredientsFatDesc();
-
+    /**
+     * Get Recipes ordered by contained fat.
+     */
+    List<Recipe> findAllByOrderByIngredientsFatDesc();
+    
+    /**
+     * Get Recipes ordered by conatined protein.
+     */
 	List<Recipe> findAllByOrderByIngredientsProteinDesc();
 }
