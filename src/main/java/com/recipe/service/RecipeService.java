f7bc9244738b97d67a85432f03cc0c87f7b3767d
dnemeth <neduabi@sztaki.mta.hu>
2019-04-04 14:20:09 +0200

    Extended recipeservice with informations

--- src/main/java/com/recipe/service/RecipeService.java
+++ src/main/java/com/recipe/service/RecipeService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.recipe.entity.Recipe;
 import com.recipe.repo.RecipeRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Well functionated recipeservice to manage a repository.
+ */
 @Service
 public class RecipeService{
 	
@@ -18,15 +20,24 @@ public class RecipeService{
 		this.recipeRepository = recipeRepository;
 	}
 
-    public String addRecipe(Recipe recipe) {
+	/**
+	 * Add recipe to repo.
+	 */
+  public String addRecipe(Recipe recipe) {
 		recipeRepository.save(recipe);
 		return "ok";
 	}
 
+	/**
+	 * Get all recipes.
+	 */
 	public List<Recipe> getRecipes() {
 		return recipeRepository.findAll();
 	}
 
+	/**
+	 * Get recipe by id.
+	 */
 	public Recipe getRecipeById(Long selectedRecipeId) {
 		return recipeRepository.findById(selectedRecipeId);
 	}
