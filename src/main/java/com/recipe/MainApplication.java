6acbb2b64cf37844453fbf505b0e1244c086af5f
dnemeth <neduabi@sztaki.mta.hu>
2019-04-06 08:00:03 +0200

    Desc. for Main class.

--- src/main/java/com/recipe/MainApplication.java
+++ src/main/java/com/recipe/MainApplication.java
@@ -3,9 +3,12 @@ package com.recipe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for running app.
+ */
 @SpringBootApplication
 public class MainApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
